99c77b63e6c65442221c62ac19eb9d1d81ee1b62
Jacob Westman <hello@jacobwestman.com>
2017-05-03 15:23:20 +0200

    Added production setup and deploy

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,28 +1,55 @@
 import webpack from 'webpack';
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMD5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
   debug: true,
   devtool: 'source-map',
   noInfo: false, //adds quite a lot of noice to the debugger - turn off for production
-  entry: [
-    path.resolve(__dirname, 'src/index')
-  ],
+  entry: {
+    vendor: path.resolve(__dirname, 'src/vendor'),
+    main: path.resolve(__dirname, 'src/index')
+  },
   target: 'web',
   output: {
     path: 'dist',
     publicPath: '',
-    filename: 'bundle.js'
+    filename: '[name].[chunkhash].js'
   },
   /* devServer: {
      contentBase: path.resolve(__dirname, 'src')
    },*/
   plugins: [
+    // Generate an external css file with a hash in the filename
+    new ExtractTextPlugin('[name].[contenthash].css'),
+    // Hash thefiles using MD5 so that their names change when their content changes.
+    new WebpackMD5Hash(),
+    // Use CommonsChunkPLugin to create a seperate bundle
+    // of vendor libraries so that they're cached seperately.
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
     // Create HTML file that includes reference to bundled JS
     new HtmlWebpackPlugin({
       template: 'src/index.html',
-      inject: true
+      inject: true,
+      // Properties you define here are available in index.html
+      // using htmlWebpackPlugin.options.varName
+      trackJSToken: '92c3a425def14ba7af181422744d5a25',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      }
     }),
     // Eliminates duplicated packages in our final bundle to keep the size as small as possible
     new webpack.optimize.DedupePlugin(),
@@ -33,7 +60,7 @@ export default {
   module: {
     loaders: [
       { test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel'] },
-      { test: /(\.css)$/, loaders: ['style', 'css'] },
+      { test: /(\.css)$/, loader: ExtractTextPlugin.extract('css?sourceMap') },
       { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
       { test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000' },
       { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
